5c730eaf669d5c679d6b151eb6f8822242e9cd35
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-04-26 16:42:16 +0200

    Header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='ct-w100 ct-h80px ct-black'>
+      <div class="ct-w80A ct-h80px ct-header">
+          <div class="ct-w10">
+              <img src="../assets/img/Logo.jpg"  alt="">
+          </div>
+          <div class="ct-header-list">
+              <a href="">Home</a>
+              <a href="">About</a>
+              <a href="">Academics</a>
+              <a href="">Courses</a>
+              <a href="">News</a>
+          </div>
+
+      </div>
+      
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/style/common.scss';
+.ct-header{
+    display:flex;
+    justify-content:space-between;
+
+    .ct-header-list{
+        display: flex;
+        align-items:center;
+
+        a{
+            margin-left:15px;
+            font-weight: bold;
+        }
+    }
+}
+
+img{
+    width:90%;
+}
+
+
+
+</style>
